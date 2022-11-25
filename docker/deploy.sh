d9da6ad859b125abf22febdae45dfd5e0924f8dd
jenya <hypnagonia>
2022-11-26 02:44:17 +0400

    ++

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# git checkout master && git pull origin master
+git checkout master && git pull origin master
+
+COPY /etc/ssl/certs/nginx-selfsigned.crt ./nginx/nginx-selfsigned.crt
+COPY /etc/ssl/private/nginx-selfsigned.key ./nginx/nginx-selfsigned.key 
 
 docker-compose build --no-cache
 docker-compose down
