5a021a7369dd64548045c9635e582959ef654b8e
jenya <hypnagonia>
2022-11-26 14:39:27 +0400

    ++

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -26,10 +26,10 @@ export const generateTicketNumberEmail = (email: string, number: string) => {
 export async function sendEmail(mail: any) {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
+  const testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
     auth: {
@@ -39,7 +39,7 @@ export async function sendEmail(mail: any) {
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail(mail);
+  const info = await transporter.sendMail(mail);
   // console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
